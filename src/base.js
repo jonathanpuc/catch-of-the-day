dc5c5e40d43d8d2186ff6a9bd09357b2b735be9a
Jonathan Pucm <jonathancoder77@outlook.com>
2017-10-10 20:01:46 +1100

    first commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCvRfvVyvk9-6UcoVsAU-3BoXlFAaSbKMg",
+    authDomain: "cotd-jonpuc.firebaseapp.com",
+    databaseURL: "https://cotd-jonpuc.firebaseio.com"
+});
+
+export default base;
